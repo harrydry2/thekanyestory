85b96d07673ed5e372db6f68fa507b20200f7182
Harry Dry <harrydry@Crowdforms-MacBook-Pro.local>
2019-01-22 21:41:02 +0000

    s

--- public/src/modules/fresh.js
+++ public/src/modules/fresh.js
@@ -4,25 +4,49 @@ const freshOuter = $(".fresh");
 const freshImg1 = $(".fresh__img1");
 const freshImg2 = $(".fresh__img2");
 const freshImg3 = $(".fresh__img3");
+const freshImg2Mob = $(".fresh__img2Mob");
+const freshImg3Mob = $(".fresh__img3Mob");
 
-console.log(freshImg1);
 // window height
-// const windowHeight = freshOuter.clientHeight;
 // const imgHeight = freshImg.clientHeight;
 // const diff = windowHeight - imgHeight;
 
-setTimeout(fresh, 600);
+if (window.innerWidth > 500) {
+  window.addEventListener("load", function(event) {
+    const freshImgHeight = freshImg1.offsetHeight;
+    const windowHeight = freshOuter.clientHeight;
+    setTimeout(
+      () => (freshImg1.style.top = `-${freshImgHeight - windowHeight}px`),
+      600
+    );
+  });
 
-console.log(freshImg1.scrollHeight);
+  freshImg1.addEventListener("transitionend", event => {
+    setTimeout(() => {
+      freshImg2.style.top = "18%";
+      freshImg2.style.opacity = "1";
+    }, 300);
+  });
 
-function fresh() {
-  freshImg1.style.bottom = "0%";
+  freshImg2.addEventListener("transitionend", event => {
+    setTimeout(() => {
+      freshImg3.style.bottom = "15%";
+      freshImg3.style.opacity = "1";
+    }, 300);
+  });
 }
 
-freshImg1.addEventListener("transitionend", event => {
-  setTimeout(() => (freshImg2.style.left = "10%"), 300);
-});
-
-freshImg2.addEventListener("transitionend", event => {
-  setTimeout(() => (freshImg3.style.right = "10%"), 100);
-});
+if (window.innerWidth < 500) {
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      freshImg2Mob.style.top = "16%";
+      freshImg2Mob.style.opacity = "0.9";
+    }, 350);
+    freshImg2Mob.addEventListener("transitionend", event => {
+      setTimeout(() => {
+        freshImg3Mob.style.bottom = "38%";
+        freshImg3Mob.style.opacity = "0.9";
+      }, 500);
+    });
+  });
+}
--- public/src/modules/isChapterRead.js
+++ public/src/modules/isChapterRead.js
@@ -1,9 +1,13 @@
 import { $, $$ } from "./bling";
+import debounce from "lodash/debounce";
 
 var mobOrLeft = window.innerWidth <= 1023 ? "mobilemenu__inner" : "leftmenu";
 var dividers = Array.from($$(".divider"));
 let dividerHeight = dividers[0].offsetHeight;
 let menuToTop = $(`.${mobOrLeft}`).getBoundingClientRect().top;
+
+// window.on("scroll", debounce(isChapterRead, 30));
+// window.on("resize", debounce(updateCoords, 30));
 
 window.on("scroll", isChapterRead);
 window.on("resize", updateCoords);
@@ -18,10 +22,27 @@ function isChapterRead() {
   if (mobOrLeft === "leftmenu") {
     counter = dividerHeight - menuToTop + 20;
   }
+  // counter for end
+  var endCounter = 0;
+  if (mobOrLeft === "leftmenu") {
+    endCounter = 200;
+  }
   dividers.forEach((divider, index) => {
     if (index > 0) {
       var lm = $(`.${mobOrLeft} > .lm00${index.toString()}`);
       if (divider.getBoundingClientRect().top < -counter) {
+        lm.classList.add("strike");
+      } else {
+        lm.classList.contains("strike") ? lm.classList.remove("strike") : null;
+      }
+    }
+    if (index === 6) {
+      var lm = $(`.${mobOrLeft} > .lm007`);
+      const endPlug = $(".endPlug");
+      if (
+        endPlug.getBoundingClientRect().top - endPlug.offsetHeight <
+        -endCounter
+      ) {
         lm.classList.add("strike");
       } else {
         lm.classList.contains("strike") ? lm.classList.remove("strike") : null;
