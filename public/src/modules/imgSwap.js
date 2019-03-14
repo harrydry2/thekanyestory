503208225631ea3976fd43a7b9d994afd2e714a5
Harry Dry <harrydry@Crowdforms-MacBook-Pro.local>
2019-03-14 23:21:47 +0000

    s

--- public/src/modules/imgSwap.js
+++ public/src/modules/imgSwap.js
@@ -8,8 +8,8 @@ function imgSwap() {
     $(".hunterIMG").src = "https://s3.amazonaws.com/harrydry/disMob.png";
     $(".bamn1").src =
       "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob1.jpg";
-    $(".bamn2").src =
-      "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob2.jpg";
+    // $(".bamn2").src =
+    //   "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob2.jpg";
     $(".bamn3").src =
       "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob3.jpg";
     $(".closerIMG").src =
